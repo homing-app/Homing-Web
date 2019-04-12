c5a375b7deb09cb4d763c0150beac3baa2f190a6
Roberto <roberto.m.aragon@gmail.com>
2019-04-12 17:27:57 +0200

    baseURL routes

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.BASE_URL || "http://localhost:3001/",
   withCredentials: true,
 })
 
