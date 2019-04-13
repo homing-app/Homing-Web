c681874422ceb77397715e1e02cf9c51b8d31a5d
Roberto <roberto.m.aragon@gmail.com>
2019-04-13 11:34:18 +0200

    font weight

--- src/style/gradients.js
+++ src/style/gradients.js
@@ -10,73 +10,73 @@ export default [
     name: 'Saint Petersburg',
     description: '#f5f7fa → #c3cfe2',
     css: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Deep Blue',
     description: '#e0c3fc → #8ec5fc',
     css: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Ripe Malinka',
     description: '#f093fb → #f5576c',
     css: 'linear-gradient(135deg, #f093fb 0%, #f5576c 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Perfect White',
     description: '#fdfbfb → #ebedee',
     css: 'linear-gradient(135deg, #E3FDF5 0%, #FFE6FA 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Near Moon',
     description: '#5ee7df → #b490ca',
     css: 'linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Wild Apple',
     description: '#d299c2 → #fef9d7',
     css: 'linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)',
-    height: 200
+    height: 600
   },
   {
     name: 'Ladoga Bottom',
     description: '#ebc0fd → #d9ded8',
     css: 'linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Sunny Morning',
     description: '#f6d365 → #fda085',
     css: 'linear-gradient(135deg, #f6d365 0%, #fda085 100%)',
-    height: 200
+    height: 600
   },
   {
     name: 'Lemon Gate',
     description: '#96fbc4 → #f9f586',
     css: 'linear-gradient(135deg, #96fbc4 0%, #f9f586 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Salt Mountain',
     description: ' #FFFEFF → #D7FFFE',
     css: 'linear-gradient(135deg, #FFFEFF 0%, #D7FFFE 100%)',
-    height: 200
+    height: 600
   },
   {
     name: 'New York',
     description: ' #fff1eb → #ace0f9',
     css: 'linear-gradient(135deg, #fff1eb 0%, #ace0f9 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Soft Grass',
     description: ' #c1dfc4 → #deecdd',
     css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
-    height: 400
+    height: 600
   },
   {
     name: 'Japan Blush',
