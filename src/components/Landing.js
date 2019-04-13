3f359735953c59283ca66c0466ea8ba10f3ec5b7
Roberto <roberto.m.aragon@gmail.com>
2019-04-13 10:45:47 +0200

    fonts

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { withAuthConsumer } from '../../contexts/AuthStore'
+
+const Lading = () => (
+  <div className="landing">
+  </div>
+)
+
+export default withAuthConsumer(Lading)
+ 
